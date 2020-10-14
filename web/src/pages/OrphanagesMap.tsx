e391649215f4591a0accb72375da181781513704
unknown <guilhermealves2596@gmail.com>
2020-10-13 23:37:35 -0300

    Map page adding
    Update .gitignore file

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import mapMarkerImg from '../images/map-marker.svg';
 import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
 
+import 'leaflet/dist/leaflet.css';
+
+import '../styles/pages/orphanages-page.css';
 
 function OrphanagesMap() {
      return (
@@ -20,7 +25,22 @@ function OrphanagesMap() {
                     </footer>
                </aside>
 
-               <div></div>
+               <Map
+                    center={[-23.5620651,-46.757523]}
+                    zoom={15}
+                    style={{ width: '100%', height:'100%' }}
+               >
+                  {/*  <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                         
+                         {/* Forma de se acessar uma variável de ambiente no react => process.env.VAR_NAME */}
+                         <TileLayer  
+                              url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                         />
+               </Map>
+
+               <Link to="" className="create-orphanage">
+                    <FiPlus size="32" color="#FFF" />
+               </Link>
           </div>
      );
 }
